d2696457976d1951a10004f0f96da5d3dabe34b8
IceDemonIX <79558933+IceDemonIX@users.noreply.github.com>
2022-06-06 18:44:11 -0500

    TOKEN= OTgzNDg5MTQ3Mjk1NjYyMTkw.GY3Cn9.G70Ml6RHMNW-q1w5qBDBi1Eyf6P0E5kPRRJdS0

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const Discord = require("discord.js")
-require("OTgzNDg5MTQ3Mjk1NjYyMTkw.GxBN7m.yvYVdfuc5LHQrlS-g79IUkGUK5AqpQ-aQGrEJw").config()
-const TOKEN = "OTgzNDg5MTQ3Mjk1NjYyMTkw.GxBN7m.yvYVdfuc5LHQrlS-g79IUkGUK5AqpQ-aQGrEJw"
+require("OTgzNDg5MTQ3Mjk1NjYyMTkw.GY3Cn9.G70Ml6RHMNW-q1w5qBDBi1Eyf6P0E5kPRRJdS0").config()
+const TOKEN = "OTgzNDg5MTQ3Mjk1NjYyMTkw.GY3Cn9.G70Ml6RHMNW-q1w5qBDBi1Eyf6P0E5kPRRJdS0"
 
 const client = new Discord.Client({ 
     intents:[
     "GUILDS",
-    "GUILD_MESSAGES"
+    "GUILD_MESSAGES",
+    "GUILD_MEMBERS" 
     ]
 })
 
@@ -20,6 +21,12 @@ client.on("messageCreate", (message) => {
     if (message.content == "hello") {
         message.reply("whats up!")
     }
+})
+
+const welcomeChannelId = "983512382670266378"
+
+client.on("guildMemberAdd", (member) => {
+member.guild.channels.cache.get(welcomeChannelId).send(`<@${member.id}> Welcome to the server!`)
 })
 
 client.login(process.env.TOKEN)
